322f7733905ec7767f7bd08e8e8ce6fb4a666a84
aiko-sahyuri <160526894+aiko-sahyuri@users.noreply.github.com>
2024-06-14 02:54:29 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>aiko sahyuri rodriguez berino </p>'; 
+    echo '<p>colonia:teresitas</p>'; 
+    echo '<P>calle:miguel idalgo</p>';
+    echo '<p>numero:666666</p>'; 
+    echo '<p>municiopio:estrella de la mañana</p>'; 
+    
+    echo '<p>telefono:81-526-9456</p>'; 
+    
+    
+    ?> 
+
+</html>
